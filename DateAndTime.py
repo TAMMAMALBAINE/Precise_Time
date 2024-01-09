90f9144bcf249c45e44f9ee79af283c62410fae2
Tammam Albaine <tammam.albaine@gmail.com>
2024-01-09 21:53:35 +0100

    Initial commit

---
+++ DateAndTime.py
@@ -0,0 +1,7 @@
+import datetime
+Now= datetime.datetime.now()
+Date= datetime.datetime.now().date()
+Time= datetime.datetime.now().time()
+print("DATE NOW :",Now)
+print("Only Date :",Date)
+print("Only Time :",Time)
